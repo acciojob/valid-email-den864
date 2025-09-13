16112e9996dbfc9f3948a07be089f754eefe9766
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-09-13 08:30:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function validEmail(str) {
   //your JS code here.
+	if(!str)return false;
+	const regex = /^\w+(?:[.-]?\w+)*@\w+(?:[.-]?\w+)*\.\w{2,3}$/;
+	return regex.test(str);
+
+
+	
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
+
+
 alert(validEmail(str));
+
